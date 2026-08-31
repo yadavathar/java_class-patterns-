721c9a66419ccadec3ad4cd6fd05a6ac36838b10
Athar Yadav <yadavathar@gmail.com>
2026-08-31 11:42:32 +0530

    Did 2 patter

--- pattern15.java
+++ pattern15.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class pattern15 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(char ch= (char) ('E'-i);ch<='E';ch++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
 }
--- pattern16.java
+++ pattern16.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class pattern16 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==n-1 || j==n-1){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
